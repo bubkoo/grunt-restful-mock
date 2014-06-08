55d7998b3945d846eb92549c3f207ab05881261c
bubkoo <bubkoo@163.com>
2014-06-08 16:54:18 +0800

    new demo: demo for route files

--- Gruntfile.js
+++ Gruntfile.js
@@ -31,19 +31,115 @@ module.exports = function (grunt) {
         // Configuration to be run (and then tested).
         mock: {
             options: {
-                debug: true
+                debug: true,
+                // 监视配置文件变化，自动重启服务
+                watch: 'grunt/mock.js'
             },
+
             victory: {
                 options: {
-                    port: '80',
-                    hostname: 'myi.vip.com'
-                }
-            },
-            userBench: {
-                options: {
-                    port: '80',
-                    hostname: 'userbench.vip.vipshop.com'
-                }
+                    port: '6001',
+                    debug: true,
+                    cookies: {},
+                    route: {
+
+                        '/api/safe_center/state': {
+                            'get': {
+                                data: {
+                                    'code': '0',
+                                    'data': {
+                                        // 累积收益，最小单位为分，100表示1元
+                                        'bind_email': '',
+                                        'bind_phone': '',
+                                        'set_wallet_pwd': true
+                                    }
+                                }
+                            }
+                        },
+
+                        '/api/fund/unbind_cash_card': {
+                            post: {
+                                delay: 3000,
+                                data: {
+                                    code: '0',
+                                    msg: ''
+                                }
+                            }
+                        },
+
+                        '/api/fund/other_bank_item': {
+                            post: {
+                                data: {
+                                    'code': '0',
+                                    'data|0-5': [
+                                        {
+                                            'agrno': '',
+                                            'card': '8558',
+                                            'card_bank': 'CNCB',
+                                            'card_name': '中信银行信用卡',
+                                            'card_type': '2',
+                                            'id': '10',
+                                            'img': 'cncb.jpg',
+                                            'user_id': '60000046'
+                                        }
+                                    ]
+                                }
+                            }
+                        },
+
+                        '/api/fund/unbind_fast_card': {
+                            post: {
+                                delay: 3000,
+                                data: {
+                                    code: '0',
+                                    msg: ''
+                                }
+                            }
+                        },
+
+                        '/api/settiing/set_cookie': {
+                            'get': {
+                                data: {
+                                    'asset': {
+                                        'is_open_account': true
+                                    }
+                                }
+                            }
+                        },
+
+                        '/api/fund/bank_item': {
+                            'get': {
+                                data: {
+                                    'code': '0',
+                                    'data|0-1': [
+                                        {
+                                            // 银行卡号
+                                            'bnkNo': '0558',
+                                            // 银行卡绑定编号
+                                            'bnkBondId': '1111',
+                                            // 银行卡名称
+                                            'bnkCard': '工商银行',
+                                            // 银行卡描述
+                                            'bnkDesc': 'desc'
+                                        }
+                                    ]
+                                }
+                            }
+                        },
+
+                        '/api/fund/unbind_w_card': {
+                            post: {
+                                delay: 3000,
+                                data: {
+                                    code: '0',
+                                    msg: ''
+                                }
+                            }
+                        }
+                    }
+                },
+                cwd: 'mock',
+                src: ['*.js', '*.yaml', '*.coffee', '*.json']
             }
         },
 
