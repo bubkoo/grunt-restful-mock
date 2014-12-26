ed07b72acbf020302009f42342b79cbdab543a37
bubkoo <bubkoo@163.com>
2014-12-26 22:04:43 +0800

    feature: formItem

--- tasks/lib/random/devel.js
+++ tasks/lib/random/devel.js
@@ -1,9 +1,22 @@
 var autoIncrementInteger = 0;
 
 module.exports = {
-    'formItem': function (key) {
-        var data = this.params;
-        return data && data[key] || '';
+    'formItem': function (keys) {
+        var data = this.params || {};
+        var result;
+
+        if (typeof keys === 'string') {
+            result = data[keys];
+        } else if (Object.prototype.toString.call(keys) === 'object Array') {
+            result = [];
+            keys.forEach(function (key) {
+                result.push(data[key]);
+            });
+        } else {
+            result = keys;
+        }
+
+        return result;
     },
 
     'increment': function (start, step) {
